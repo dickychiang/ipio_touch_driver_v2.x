144cee339ad4a6ef125a123bb9b77eafbe65c93a
hychiang <dickychiang73@gmail.com>
2019-08-02 11:35:03 +0800

    Add release note v2.0.1.2

--- ilitek.h
+++ ilitek.h
@@ -102,7 +102,7 @@
 #include "sync_write.h"
 #endif
 
-#define DRIVER_VERSION			"2.0.1.1"
+#define DRIVER_VERSION			"2.0.1.2"
 
 /* Options */
 #define TDDI_INTERFACE			BUS_SPI /* BUS_I2C(0x18) or BUS_SPI(0x1C) */
